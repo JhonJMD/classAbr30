a4c4e23c3747d30fc09e021692ae3e2522364afc
JhonJMD <152086681+JhonJMD@users.noreply.github.com>
2024-05-03 10:41:37 -0500

    safe commit

--- heroes.js
+++ heroes.js
@@ -44,3 +44,83 @@ console.log({propiedades});
 //Obtener los valores del Objeto Literal
 const valores = Object.values(marvel);
 console.log({propiedades, valores});
+// " #Concatenar Arrays con Spread
+// crea una funcion que tome dos arrays como parametros
+// y devuelva un nuevo array que contenga todos
+// los elementos de ambos arrays. Utiliza el operador
+// de propagacion
+
+let frutas1 = ["manzana", "pera", "piña"];
+let frutas2 = ["Banano", "quiwi"];
+
+const combinarFrutas = (frutas1, frutas2) => {
+    let frutas3 = [...frutas1, ...frutas2];
+    return frutas3;
+};
+
+console.log(combinarFrutas(frutas1, frutas2));
+
+// EJERCICIO 2
+// Crea una funcion que tome un objeto como parametro
+// lo clone utilizando el operador de propagacion y
+// modifique una propiedad específica
+
+const usuario = {
+    nombre: "Juan",
+    apellido: "perez",
+    id: 102577353,
+};
+
+const objeto = (usuario, nuevaPropiedad) => {
+    let usuarioClon = { ...usuario };
+    usuarioClon = { ...usuarioClon, ...nuevaPropiedad };
+
+    return usuarioClon;
+};
+const nuevoUsuario = objeto(usuario, { nombre: "Pedro" });
+console.log(usuario);
+console.log(nuevoUsuario);
+
+// EJERCICIO #3
+//Crea una funcion flecha que tome una cantidad
+//variable de argumentos y devuelva la suma
+// de esos numeros
+
+const otro = (...alguOtro) => {
+    let suma = 0;
+    for (let i = 0; i < alguOtro.length; i++) {
+    suma += alguOtro[i];
+}
+    return suma;
+};
+resultado = otro(1, 2, 3, 4, 5);
+console.log(resultado);
+
+// EJERCICIO #4
+//Crea una funcion que tome dos objetos como parametros
+// y devuelva un nuevo objeto que combine las propiedades de ambos
+//objetos. si hay propiedades con el mismo nombre, el valor
+// del segundo objeto debería prevalecer
+
+const mascota1 = {
+    nombre: "michi",
+    edad: "1 año",
+    color: "negro",
+    raza:"persa"
+};
+const mascota2 = {
+    nombre: "michi",
+    edad: "1 año",
+    color: "blanco",
+    tipo: "felino",
+};
+
+const mascotas = (mascota1, mascota2) => {
+    let mascota = { ...mascota1, ...mascota2 };
+    return mascota;
+};
+
+console.log(mascotas(mascota1, mascota2));
+
+
+
